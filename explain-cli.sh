34de237eb6d14f154129871fca3dedee5a6171c1
Cemre HASIRCI <cemrehasirci@hotmail.com>
2025-07-24 16:23:16 +0300

    updated

--- explain-cli.sh
+++ explain-cli.sh
@@ -8,25 +8,25 @@ else
 fi
 
 #dizin boyutu
-DIZIN_BOYUTU=$(du -ch $dizin |grep total)
+DIZIN_BOYUTU=$(du -ch $dizin | grep total)
 
 #dosya sayısı
-DOSYA_SAYISI=$( ls $dizin | wc -l )
+DOSYA_SAYISI=$(ls $dizin | wc -)
 
 #en büyük boyutlu dosya ve boyutu
-BUYUK_DOSYA=$( du -sh $dizin )
+BUYUK_DOSYA=$(du -sh $dizin)
 
 #en küçük boyutlu dosya ve boyutu
-KUCUK_DOSYA=$( du -ah $dizin | sort -r | head -n 1 )
+KUCUK_DOSYA=$(du -ah $dizin | sort -r | head -n 1)
 
 #klasör sayısı
-KLASOR_SAYISI=$( find . $dizin -type f | wc -l )
+KLASOR_SAYISI=$(find . $dizin -type f | wc -l)
 
 #dizin sahibi
-SAHIBI=$( stat -c %U $dizin )
+SAHIBI=$(stat -c %U $dizin)
 
 #içindeki dosyalar ve uzantıları
-DOSYALARI=$( find . $dizin -type f -name "*" | grep "/home" )
+DOSYALARI=$(find . $dizin -type f -name "*" | grep "/home")
 
 
 echo "Dizin boyutu: "
